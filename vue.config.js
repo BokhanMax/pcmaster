ebd157e2f5843c6e8d1ee22d5e5e754b956ad3da
Max B <bokhanworks@gmail.com>
2023-10-19 10:39:21 +0300

    add pug template lang

--- vue.config.js
+++ vue.config.js
@@ -2,3 +2,23 @@ const { defineConfig } = require('@vue/cli-service')
 module.exports = defineConfig({
   transpileDependencies: true
 })
+module.exports = {
+  configureWebpack: {
+    module: {
+      rules: [
+        {
+          test: /\.pug$/,
+          oneOf: [
+            {
+              resourceQuery: /^\?vue/,
+              use: ['pug-plain-loader']
+            },
+            {
+              use: ['raw-loader', 'pug-plain-loader']
+            }
+          ]
+        }
+      ]
+    }
+  }
+}
